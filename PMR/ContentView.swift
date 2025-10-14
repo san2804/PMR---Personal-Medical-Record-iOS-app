6bceebd447fc7f8a386e1f9cda3bea96394ca83d
san2804 <kmsandildm@gmail.com>
2025-10-14 09:52:47 +0530

    Initial commit

---
+++ PMR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PMR
+//
+//  Created by Sandil on 2025-10-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PMR/PMRApp.swift
@@ -0,0 +1,17 @@
+//
+//  PMRApp.swift
+//  PMR
+//
+//  Created by Sandil on 2025-10-14.
+//
+
+import SwiftUI
+
+@main
+struct PMRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
